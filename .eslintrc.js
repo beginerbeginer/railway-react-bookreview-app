76f274e803970b2e4d88996f50eef4ec6755591b
beginerbeginer <miyabi6125@gmail.com>
2023-08-28 17:53:46 +0900

    編集：eslintによるコードの整形

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+const config = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  settings: {
+    react: {
+      version: 'detect', // 自動的にReactのバージョンを検出する
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'jest', 'import'],
+  rules: {
+    'react/jsx-uses-react': 'off', // ReactのJSX変換時にReactのインポートが不要にする
+    'react/react-in-jsx-scope': 'off', // JSXを使用する際にReactのスコープ内であることを要求しない
+    'prettier/prettier': 'error', // Prettierのフォーマットルールに違反した場合にエラーを出力する
+    'jest/consistent-test-it': ['error', { fn: 'test' }], // Jestでのテスト関数名を一貫性を持って'test'とする
+    'jest/require-top-level-describe': ['error'], // Jestのテストケースをトップレベルのdescribeブロック内に要求する
+    'import/order': [
+      // インポートの順序を指定したルールに従って整理する
+      'error',
+      {
+        'newlines-between': 'always', // インポートのグループの間には常に新しい行を挿入する
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'], // インポートの順序を定義するグループ
+        alphabetize: { order: 'asc', caseInsensitive: true }, // インポートをアルファベット順（大文字・小文字を区別しない）にする
+      },
+    ],
+  },
+}
+
+module.exports = config
