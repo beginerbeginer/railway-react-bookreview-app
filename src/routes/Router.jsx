3ce39266f7f198e2629463e7651b671337e46c0e
beginerbeginer <miyabi6125@gmail.com>
2023-09-20 14:19:16 +0900

    [#3] 編集：ルーティング

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,19 +1,32 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate, Link, useLocation } from 'react-router-dom'
 
 import { Signin } from '../Pages/Signin.jsx'
+import { Signup } from '../Pages/Signup.jsx'
 
 export const AppRouter = () => {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <Signin />
-          <Routes>
-            <Route path="/login" element={<Signin />} />
-            <Route path="/" element={<Navigate to="/signin" />} />
-          </Routes>
+          <RoutesWithLinks />
         </header>
       </div>
     </Router>
+  )
+}
+
+const RoutesWithLinks = () => {
+  // 現在のルート（URL）を取得する
+  const location = useLocation()
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      {location.pathname === '/login' && <Link to="/signup">登録はこちらから</Link>}
+    </>
   )
 }
