7ea5a94670c7ceeae1c391b2f674460af361c1d1
beginerbeginer <miyabi6125@gmail.com>
2023-09-21 15:58:26 +0900

    [#3] 追加：ライブラリ

--- src/__tests__/Signup.test.js
+++ src/__tests__/Signup.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 
 import { Signup } from '../Pages/Signup.jsx'
 
-describe('Signin', () => {
+describe('Signup', () => {
   it('ログイン画面に必要なコンポーネントが存在する', () => {
     render(<Signup />)
 
@@ -15,6 +15,5 @@ describe('Signin', () => {
     expect(screen.getByText('登録')).toBeInTheDocument()
 
     expect(screen.getByLabelText('画像を追加')).toBeInTheDocument()
-    expect(screen.getByTestId('image-preview')).toBeInTheDocument()
   })
 })
